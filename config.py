ed183a7214db3c8ac7bb7df79eecefb39e47764d
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-17 20:12:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import getenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "BQBXF2DOuDBLqJokSx9T0MRqVty2J2NwS7XudqGxCdGD1LFkHSwr8qXvFKwf00z0BoD5_HQB-l6puUksVrtjSDPHdsDnrUamcRPKHs26O17Rw9S4TifU-MZkWFbKMm8JB6YoUN4WAtQVcqqDqlYOh-T9Ci1gcKJ9v2CCxXH6r2ipXPYSW9lL0u3-Dm7AbslDbPu5FIoWBlFqYhA9wMN23Ks54DTI7BOLnJ8FStGwB_e0-l2fJKxvp7TzV45VCQGZYNTDv_w7irg1G_98ukz7Udxb6g4D0Zd86IHSXTtwDZf_konX7uWrt6LMetYoSk4o6UenfKRrv55wofvRSOkpSMOEAAAAAZuXmKgA")
 BOT_TOKEN = getenv("BOT_TOKEN", "1810353153:AAHLFmOhtT2rJF25SHvs4k6BScNJkp2P-6k")
@@ -15,4 +15,5 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+# Provide a default value for SUDO_USERS
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1137799257").split()))
